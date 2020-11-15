81d85a69b3092d1d32263891d4026518ce70a7f3
Elvis3141 <37953567+Elvis3141@users.noreply.github.com>
2020-11-15 09:02:41 +0200

    Update SearchQueries.java

--- src/project1/SearchQueries.java
+++ src/project1/SearchQueries.java
@@ -13,7 +13,6 @@ import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.store.FSDirectory;
 import txtparsing.TXTParsing;
 import utils.IO;
-
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -21,7 +20,6 @@ import java.nio.file.Paths;
 
 public class SearchQueries {
    private String queriesFile =  "docs//queries.txt";
-
 
     public SearchQueries(){
         try{
@@ -33,51 +31,26 @@ public class SearchQueries {
             IndexReader indexReader = DirectoryReader.open(FSDirectory.open(Paths.get(indexLocation)));
 
             //Creates a searcher searching the provided index, Implements search over a single IndexReader.
-
             Analyzer analyzer = new EnglishAnalyzer();
-
-            //Similarity similarity = new BM25Similarity();
-
             IndexSearcher indexSearcher = new IndexSearcher(indexReader);
-
             indexSearcher.setSimilarity(new BM25Similarity());
-            //System.out.println("indexSearcher  "+indexSearcher.collectionStatistics(field).docCount());
             // define which analyzer to use for the normalization of user's query
 
             // create a query parser on the field "contents"
             QueryParser parser = new QueryParser(field, analyzer);
-
             String[] queries = TXTParsing.parseQueries(queriesFile);
 
             /**query-number    Q0  document-id rank    score   Exp*/
-
             String results  = "";
-            //for(String strQuery : queries){
             for (int i=0; i<queries.length; i++){
                 // parse the query according to QueryParser
                 Query query = parser.parse(queries[i]);
-                //System.out.println("Searching for: " + query.toString(field));
-
                 // search the index using the indexSearcher for k top searchers
                 TopDocs queryResults = indexSearcher.search(query, 20);
-
-               /* TopScoreDocCollector collector = TopScoreDocCollector.create(20);
-                indexSearcher.search(query, collector);
-                ScoreDoc[] hits = collector.topDocs().scoreDocs;
-                System.out.println("Found " + hits.length + " hits.");
-                for(int j=0;j<hits.length;++j) {
-                    int docId = hits[j].doc;
-                    Document d = indexSearcher.doc(docId);
-                    System.out.println((j + 1) + ". " + d.get("title"));
-                }*/
-
 
                 ScoreDoc[] hits = queryResults.scoreDocs;
                 long numTotalHits = queryResults.totalHits;
                 //String docID = indexSearcher.doc();
-                //System.out.println("numTotalHits "+numTotalHits);
-
-                //System.out.println(numTotalHits + " total matching documents");
 
                 for(int j=0; j<hits.length; j++){
                     Document hitDoc = indexSearcher.doc(hits[j].doc);
@@ -88,12 +61,8 @@ public class SearchQueries {
                     System.out.println(hitDoc.get("text"));
                     System.out.println("\n");
                 }
-
-
             }
-            //System.out.println(results);
             IO.WriteToFile(results, "docs//results_file.txt", "C://Users//Elvis//Desktop//Anaktish//trec_eval//results_file.txt");
-
 
             //Close indexReader
             indexReader.close();
@@ -105,5 +74,4 @@ public class SearchQueries {
     public static void main(String[] args){
         SearchQueries searcherDemo = new SearchQueries();
     }
-
 }
