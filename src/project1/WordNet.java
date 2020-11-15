bcc8b432b4e48f2b6307d3eb0434d9fda8d732c5
Elvis3141 <37953567+Elvis3141@users.noreply.github.com>
2020-11-15 08:56:08 +0200

    Update WordNet.java

--- src/project1/WordNet.java
+++ src/project1/WordNet.java
@@ -18,58 +18,14 @@ import java.util.Map;
 
 public class WordNet {
 
-
-
-
-
-
-
-public static void main(String[]args) throws IOException, ParseException {
-
-
-    File file = new File("WordNet/wn_s.pl");
-    InputStream stream = new FileInputStream(file);
-    Reader rulesReader = new InputStreamReader(stream);
-    SynonymMap.Builder parser = null;
-    parser = new WordnetSynonymParser(true, true, new WhitespaceAnalyzer());
-    //((WordnetSynonymParser) parser).add(rulesReader);
-    ((WordnetSynonymParser) parser).parse(rulesReader);
-    SynonymMap synonymMap = parser.build();
-
-
-
-    /*SynonymMap map = new SynonymMap(new FileInputStream("samples/fulltext/wn_s.pl"));
-    for (int i = 0; i &lt; words.length; i++) {
-             String[] synonyms = map.getSynonyms(words[i]);
-            System.out.println(words[i] + ":" + java.util.Arrays.asList(synonyms).toString());
-     }*/
-
-
-
-
-/*    Map<String, String> sffargs = new HashMap<>();
-    sffargs.put("synonyms", "WordNet//wn_s.txt");
-    sffargs.put("format", "wordnet");
-
-    CustomAnalyzer.Builder builder = CustomAnalyzer.builder().withTokenizer(WhitespaceTokenizerFactory.class)
-            .addTokenFilter(SynonymGraphFilterFactory.class, sffargs);*/
-
-}
-
-
-
-  /*  public CustomAnalyzer readWordNet() throws IOException {
-        Map<String, String> sffargs = new HashMap<>();
-        sffargs.put("synonyms", "synonyms-wn.txt");
-        sffargs.put("format", "wordnet");
-
-        CustomAnalyzer.Builder builder = CustomAnalyzer.builder()
-                .withTokenizer(WhitespaceTokenizerFactory.class)
-                .addTokenFilter(SynonymGraphFilterFactory.class, sffargs);
-        return builder.build();
-    }*/
-
-
-
+    public static void main(String[]args) throws IOException, ParseException {
+        File file = new File("WordNet/wn_s.pl");
+        InputStream stream = new FileInputStream(file);
+        Reader rulesReader = new InputStreamReader(stream);
+        SynonymMap.Builder parser = null;
+        parser = new WordnetSynonymParser(true, true, new WhitespaceAnalyzer());
+        ((WordnetSynonymParser) parser).parse(rulesReader);
+        SynonymMap synonymMap = parser.build();
+    }
 
 }
